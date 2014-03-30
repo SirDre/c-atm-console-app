0778749f46f2de87d9b2e4b236ed89a34795f4e1
Andre Gardiner <andre@andorum.com>
2014-03-30 13:18:50 -0500

    Update main.c
    Define Filing/Storage functions for atm vault, customer info and log info.

--- main.c
+++ main.c
@@ -30,3 +30,108 @@ typedef struct log
 	float opactivity;
     hlist *next;
 }logs;
+
+
+
+
+
+
+
+
+
+//FILING
+ 
+//--------------------------------------------------------------------------------------------------//
+//Writes Funds added to ATM Total Funds to file
+void write_vaultdb(atm cash)
+{
+    FILE *f_cash = fopen("vault.txt", "w");
+    fprintf(f_cash,"%6.2f",cash.money );
+    fclose(f_cash);
+}
+//--------------------------------------------------------------------------------------------------//
+//Reads ATM Total Funds available from file
+atm read_vaultdb()
+{
+    float money;
+    float balance;
+ 
+    FILE *f_cash = fopen("vault.txt", "r");
+    if (f_cash == NULL)
+    {
+        FILE *f_cash = fopen("vault.txt", "w");
+        fscanf(f_cash,"%f",&balance);
+        return banknotes(balance);
+    }
+    else
+        fscanf(f_cash,"%f", &money);
+    return banknotes(money);
+}
+//--------------------------------------------------------------------------------------------------//
+//Reads Customers information from file ie. account no, pin, total funds, account type, and status
+void read_custdb(ulist header)
+{
+    int i,pin,type,state,userid;
+    float sum;
+	
+    FILE *f_clientes= fopen("customers.txt", "r"); 
+    int res = fscanf(f_clientes, "%d\t\t%d\t\t%f\t\t%d\t\t%d\n", &userid,&pin,&sum,&type,&state);
+    for (i = 0; res > 0; i++)
+    {
+        create_user(header,userid,pin,sum,type,state);
+        res = fscanf(f_clientes, "%d\t\t%d\t\t%f\t\t%d\t\t%d\n", &userid,&pin,&sum,&type,&state);
+    }
+    fclose (f_clientes);
+}
+//--------------------------------------------------------------------------------------------------//
+//Writes customers banking details to file ie. account no, pin, total funds, account type, and status
+void write_custdb(ulist header)
+{
+    ulist l=header->next;
+    FILE *f_clientes = fopen("customers.txt", "w");
+ 
+    while (l)
+    {
+        fprintf (f_clientes, "%d\t\t%d\t\t%6.2f\t\t%d\t\t%d\n", l->userid,l->pin,l->sum,l->type,l->state); 
+        l=l->next;
+    }
+    fclose(f_clientes);
+}
+//--------------------------------------------------------------------------------------------------//
+//Checks and Reads Activity logs to and from File
+void read_logdb(hlist headerh)
+{
+    int nop,nuid,i;
+	float ndest;
+    hlist l = headerh->next;
+
+    FILE *f_hist = fopen("logs.txt", "r");
+    if (f_hist == NULL)
+    {
+        FILE *f_hist = fopen("logs.txt", "w");
+    }
+    else
+    {
+        int res = fscanf(f_hist, "%d %d %f\n", &nop,&nuid,&ndest);
+        for (i = 0; res > 0; i++)
+        {
+            create_log(headerh,nop,nuid,ndest);
+            res = fscanf(f_hist, "%d %d %f\n", &nop,&nuid,&ndest);
+        }
+        fclose (f_hist);
+    }
+}
+//--------------------------------------------------------------------------------------------------//
+//Writes Activity logs to File
+void write_logdb(hlist headerh)
+{
+    hlist l=headerh->next;
+    FILE *f_hist= fopen("logs.txt", "w");
+ 
+    while (l)
+    {
+        fprintf (f_hist,"%d %d %6.2f\n",l->operation,l->opuserid,l->opactivity); 
+        l=l->next;
+    }
+    fclose(f_hist);
+}
